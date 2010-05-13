205b58a3a31efdd6341e1f3e689407663564b4fe
Tim Lowrimore <tlowrimore@coroutine.com>
2010-05-13 14:34:24 -0500

    made the plugin Rails 2.X compatible

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
 require 'rails_generator'
 require 'rails_generator/scripts/generate'
 
-Rails::Generator::Scripts::Generate.new.run(["simple_navigation", :destination => RAILS_ROOT])
+Rails::Generator::Scripts::Generate.new.run(["simple_navigation"], :destination => Rails.root)
