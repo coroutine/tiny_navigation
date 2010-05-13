dabf4d5a2f0a7de14e9afb25334bb9e9b787fbdd
Tim Lowrimore <tlowrimore@coroutine.com>
2010-05-13 16:49:08 -0500

    updated the tests

--- test/navigation_test.rb
+++ test/navigation_test.rb
@@ -4,15 +4,46 @@ require 'simple_navigation/item'
 
 class NavigationTest < ActiveSupport::TestCase
   
-  test "can create an empty navigation" do
-    assert Coroutine::SimpleNavigation::Navigation.new(:main, "tests")
+  def setup
+    @controller_class     = Struct.new(:controller_name)
+    @current_controller   = @controller_class.new("tests")
   end
   
   test "can create a navigation with an item" do
-    nav = Coroutine::SimpleNavigation::Navigation.new(:main, "tests") do
+    nav = navigation do
       item "Foos", :to => "foos#index"
     end
     
     assert_equal nav.items.length, 1
+  end
+  
+  test "has correct selected item in one level of nesting" do
+    nav = navigation do
+      item "Tests", :to => "tests"
+    end
+    
+    assert_equal nav.selected.length, 1
+    assert_equal nav.selected.first.name, "Tests"
+  end
+  
+  test "has correct selected items in multiple levels of nesting" do
+    nav = navigation do
+      item "Foos", :to => "foos#index" do
+        item "Tests", :to => "tests#index"
+      end
+    end
+    
+    assert_equal nav.selected.length, 2
+    assert_equal nav.selected.map(&:name), ["Foos", "Tests"]
+  end
+  
+  # ------------------------------------------------------------------------------
+  # Helpers
+  # ------------------------------------------------------------------------------
+  
+  private
+  
+  def navigation(name = :main, current_controller = @current_controller, &block)
+    Coroutine::SimpleNavigation::Navigation.new(name, current_controller, &block)
   end
 end
